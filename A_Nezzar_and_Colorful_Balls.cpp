2d1c5b94f4965144afafd1b3c3c854671de88d00
Anand Singh Kushwaha <64066654+FlameKaiser604@users.noreply.github.com>
2021-01-29 19:59:39 +0530

    Add files via upload

---
+++ A_Nezzar_and_Colorful_Balls.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+     int max_count = 1, res = arr[0], curr_count = 1; 
+    for (int i = 1; i < n; i++) { 
+        if (arr[i] == arr[i - 1]) 
+            curr_count++; 
+        else { 
+            if (curr_count > max_count) { 
+                max_count = curr_count; 
+                res = arr[i - 1]; 
+            } 
+            curr_count = 1; 
+        } 
+    }
+      if (curr_count > max_count) 
+    { 
+        max_count = curr_count; 
+         
+    }
+   cout<<max_count<<endl;
+}
+return 0;
+}
+
